260546d23921412bfd958bcdd1325852e0b82c05
KaryanthePierrot <mesquita.westesoficial@gmail.com>
2025-05-26 22:19:49 -0300

    1?

--- src/main/java/br/studio/pilates/controller/AlunoController.java
+++ src/main/java/br/studio/pilates/controller/AlunoController.java
@@ -2,18 +2,14 @@ package br.studio.pilates.controller;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,9 +25,6 @@ public class AlunoController {
 
 	@Autowired
 	private AlunoService alunoService;
-
-	@GetMapping("aluno")
-	public List<Aluno> listar() {
 
 	@GetMapping("aluno")
 	public List<Aluno> listar() {
@@ -55,7 +48,7 @@ public class AlunoController {
 	}
 
 	@GetMapping("aluno/cpf/{cpf}")
-	public Optional<Aluno> getByCpfAluno(@PathVariable("cpf") Long cpf) {
+	public Optional<Aluno> getByCpfAluno(@PathVariable("cpf") String cpf) {
 		return alunoService.getByCpf(cpf);
 	}
 
