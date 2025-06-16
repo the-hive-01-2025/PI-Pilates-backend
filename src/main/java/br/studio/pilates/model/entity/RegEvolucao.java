97754e30edde6fe7a73a707a59941c16c9e6dbd1
KaryanthePierrot <mesquita.westesoficial@gmail.com>
2025-06-16 19:52:43 -0300

    lombok add e inutilidades removidas

--- src/main/java/br/studio/pilates/model/entity/RegEvolucao.java
+++ src/main/java/br/studio/pilates/model/entity/RegEvolucao.java
@@ -8,6 +8,18 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+
 @Document(collection = "registrosEvolucao")
 public class RegEvolucao{
 
