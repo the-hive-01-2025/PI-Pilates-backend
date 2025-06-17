59ecd5f9b7bfdd9eaf65887fa0d7c06078d4530a
Lophezz <lopes.matheus334@gmail.com>
2025-06-17 02:10:20 -0300

    Documentação

--- src/main/java/br/studio/pilates/config/UsuarioDataLoader.java
+++ src/main/java/br/studio/pilates/config/UsuarioDataLoader.java
@@ -1,28 +1,28 @@
-package br.studio.pilates.config;
+// package br.studio.pilates.config;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import br.studio.pilates.model.entity.Usuario;
-import br.studio.pilates.model.entity.repository.UsuarioRepository;
+// import br.studio.pilates.model.entity.Usuario;
+// import br.studio.pilates.model.entity.repository.UsuarioRepository;
 
-@Configuration
-public class UsuarioDataLoader {
+// @Configuration
+// public class UsuarioDataLoader {
 
-    @Bean
-    CommandLineRunner loadUsuarios(UsuarioRepository usuarioRepository) {
-        return args -> {
-            if (usuarioRepository.count() == 0) {
-                usuarioRepository.saveAll(List.of(
-                        new Usuario(null, "recepção", "recepcao@teste.com", "123456", "RECEPCAO"),
-                        new Usuario(null, "user", "usuario@teste.com", "123456", "USER"),
-                        new Usuario(null, "instrutor", "instrutor@teste.com", "123456", "INTRUTOR"),
-                        new Usuario(null, "admin", "admin@teste.com", "123456", "ADMIN")));
-                System.out.println("Planos padrão inseridos no banco.");
-            }
-        };
-    }
-}
+//     @Bean
+//     CommandLineRunner loadUsuarios(UsuarioRepository usuarioRepository) {
+//         return args -> {
+//             if (usuarioRepository.count() == 0) {
+//                 usuarioRepository.saveAll(List.of(
+//                         new Usuario(null, "recepção", "recepcao@teste.com", "123456", "RECEPCAO"),
+//                         new Usuario(null, "user", "usuario@teste.com", "123456", "USER"),
+//                         new Usuario(null, "instrutor", "instrutor@teste.com", "123456", "INTRUTOR"),
+//                         new Usuario(null, "admin", "admin@teste.com", "123456", "ADMIN")));
+//                 System.out.println("Planos padrão inseridos no banco.");
+//             }
+//         };
+//     }
+// }
